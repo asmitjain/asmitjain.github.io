ee2175f2f2030c6a7971696883b0f87007acbe20
Asmit Jain <55670049+asmitjain@users.noreply.github.com>
2019-09-24 23:00:57 -0400

    Delete thebox.js

--- js/thebox.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {toggleStickyTopBar()};
-
-function toggleStickyTopBar() {
-  if (document.body.scrollTop > 720 || document.documentElement.scrollTop > 720) {
-    document.getElementById('sticky-header').classList.add('top-navigation-shadow');
-  } else {
-    document.getElementById('sticky-header').classList.remove('top-navigation-shadow');
-  }
-}
